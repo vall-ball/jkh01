924e9023b3198f0d4587aab4cc6a235d5db39eb6
vall-ball <vallball1981@gmail.com>
2021-10-29 12:32:54 +0700

    Project creation 29.10.2021

---
+++ src/main/java/ru/vallball/jkh01/Jkh01Application.java
@@ -0,0 +1,13 @@
+package ru.vallball.jkh01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jkh01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jkh01Application.class, args);
+	}
+
+}
---
+++ src/main/java/ru/vallball/jkh01/config/SecurityConfig.java
@@ -0,0 +1,5 @@
+package ru.vallball.jkh01.config;
+
+public class SecurityConfig {
+
+}
